6446aa5b1f0b75d292aa341055e3210de14f7f13
David Fischer <fischerdm1@outlook.com>
2025-11-01 09:23:01 +0100

    comment out methods with bugs

--- examples/main.py
+++ examples/main.py
@@ -1,13 +1,12 @@
 from bollinger_bands import Strategy, DataFetcher
 
-fetcher = DataFetcher()
-
-data = fetcher.fetch_daily_data(['AAPL', 'IBM'], '2024-01-01', '2024-12-31')
-print(data.head())
-
-
 if __name__ == "__main__":
-    print("Running Bollinger Bands + Relative Strength analysis...")
-    strategy = Strategy(ticker='ACWI', benchmark='^GSPC')
-    strategy.run_analysis()
+    # print("Running Bollinger Bands + Relative Strength analysis...")
+    # strategy = Strategy(ticker='ACWI', benchmark='^GSPC')
+    # strategy.run_analysis()
+    fetcher = DataFetcher()
+
+    data = fetcher.fetch_daily_data(['AAPL', 'IBM'], '2024-01-01', '2024-12-31')
+    print(data.head())
+
 
